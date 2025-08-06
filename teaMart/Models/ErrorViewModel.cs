d939c4b657acd0fd268426496e97f90c9c928cc1
Robert-J-WANG <robertwang6486@gmail.com>
2025-08-06 18:27:56 +1200

    Add project files.

---
+++ teaMart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace teaMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

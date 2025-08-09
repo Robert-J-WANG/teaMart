d55677b0449fc034b10341b0b6eff51888506515
Robert-J-WANG <robertwang6486@gmail.com>
2025-08-09 22:38:24 +1200

    admin dashboard 2

--- teaMart/Controllers/AdminController.cs
+++ teaMart/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using teaMart.Models;
 
 namespace teaMart.Controllers
@@ -10,19 +11,50 @@ namespace teaMart.Controllers
         // 依赖注入HttpContextAccessor的接口
         private readonly IHttpContextAccessor _httpContextAccessor;
 
+        // 构造函数注入依赖
+        public AdminController(dbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            _dbContext = dbContext;
+            _httpContextAccessor = httpContextAccessor;
+        }
 
-
-
-
+        // 后台管理首页的信息展示方法
         public IActionResult Index()
         {
-            
+            //1， 今天平台的收入
+            //var startOfToday = DateTime.Now.Date; // 获取今天的开始时间
+            var startOfToday = DateTime.Parse("2024-09-26"); // 获取2024-9-26的0点
+            var endOfToday = startOfToday.AddDays(1); // 获取明天的开始时间
+            ViewBag.todayMoney = _dbContext.Orders
+                .Where(o => o.IsPay == 1 && o.Createtime >= startOfToday && o.Createtime < endOfToday)
+                .Sum(o => (Decimal?)o.SumPrice) ?? 0; // 计算今天的总收入
+
+            //2， 本周的收入
+            DateTime dateTime = DateTime.Now;
+            DateTime startOfWeek = dateTime.AddDays(1 - Convert.ToInt32(dateTime.DayOfWeek.ToString("d"))).Date; // 周一
+            DateTime endOfWeek = startOfWeek.AddDays(7); // 下周一
+            ViewBag.startOfWeek = startOfWeek; // 本周开始时间
+            ViewBag.endOfWeek = endOfWeek.AddDays(-1); // 本周结束时间（周日）
+
+            ViewBag.weekMoney = _dbContext.Orders
+                .Where(o => o.IsPay == 1 && o.Createtime >= startOfWeek && o.Createtime < endOfWeek)
+                .Sum(o => (Decimal?)o.SumPrice) ?? 0; // 计算本周的总收入
+
+            //3， 今年的收入
+            DateTime startOfYear = new DateTime(DateTime.Now.Year, 1, 1, 0, 0, 0); // 获取今年的开始时间
+            DateTime endOfYear = new DateTime(DateTime.Now.Year + 1, 1, 1, 0, 0, 0); // 获取明年开始时间
+            ViewBag.yearMoney = _dbContext.Orders
+                .Where(o => o.IsPay == 1 && o.Createtime >= startOfYear && o.Createtime < endOfYear)
+                .Sum(o => (Decimal?)o.SumPrice) ?? 0; // 计算今年的总收入
+
+            //4， 今天的订单详情（取前10条）
+            ViewBag.orderList = _dbContext.Orders.Include(o => o.UidNavigation)
+                .Where(o => o.Createtime >= startOfToday && o.Createtime < endOfToday)
+                .OrderByDescending(o => o.Id)
+                .Take(10)
+                .ToList(); // 获取今天的订单详情
+
             return View();
-
-
-            
-                
-            
         }
     }
 }
